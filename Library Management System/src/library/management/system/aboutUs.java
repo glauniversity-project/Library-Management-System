c8fe23c3bdb625f2d3f9e129d1de83474b43b2f9
PawanPandey007 <70752928+PawanPandey007@users.noreply.github.com>
2021-12-09 23:23:04 +0530

    Fixed contact us link problem.

--- Library Management System/src/library/management/system/aboutUs.java
+++ Library Management System/src/library/management/system/aboutUs.java
@@ -1,23 +1,28 @@
 package library.management.system;
 
 import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+ 
+import javax.swing.JFrame;
 import javax.swing.*;
-import javax.swing.border.*;
+import javax.swing.SwingUtilities;
 
-public class aboutUs extends JFrame{
-
-	private JPanel contentPane;
-
-        public static void main(String[] args) {
-            new aboutUs().setVisible(true);			
-	}
+public class aboutUs extends JFrame {
     
-        public aboutUs() {
-            
+    public JPanel contentPane;
+    
+    public JHyperlink linkEmail1 = new JHyperlink("Pawan Pandey");
+    public JHyperlink linkEmail2 = new JHyperlink("Yash Nigam");
+    public JHyperlink linkEmail3 = new JHyperlink("Yash Gupta");
+    public JHyperlink linkEmail4 = new JHyperlink("Rajat Pandey");
+     
+    public aboutUs() throws HeadlessException {
+         
             super("Library Management System");
             setBackground(new Color(173, 216, 230));
-            setBounds(700, 100, 600, 900);
-		
+            setBounds(700, 100, 800, 600);
+            setTitle("About Us");
             contentPane = new JPanel();
             setContentPane(contentPane);
             contentPane.setLayout(null);
@@ -25,54 +30,53 @@ public class aboutUs extends JFrame{
             JLabel l3 = new JLabel("Library");
             l3.setForeground(new Color(0, 250, 154));
             l3.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 34));
-            l3.setBounds(250, 40, 150, 55);
+            l3.setBounds(340, 40, 150, 55);
             contentPane.add(l3);
 
             JLabel l4 = new JLabel("Mangement System");
             l4.setForeground(new Color(127, 255, 0));
             l4.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 34));
-            l4.setBounds(160, 90, 405, 40);
+            l4.setBounds(250, 90, 405, 40);
             contentPane.add(l4);
-
-           
-
-
-            JLabel l6 = new JLabel("Developed By : Yash Nigam");
-            JLabel l11 = new JLabel("Rajat Pandey");
-            JLabel l12 = new JLabel("Yash Gupta");
-            JLabel l13 = new JLabel("Pawan Pandey");
-            l6.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l6.setBounds(70, 200, 600, 35);
-            l11.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l11.setBounds(290, 240, 600, 35);
-            l12.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l12.setBounds(290, 280, 600, 35);
-            l13.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l13.setBounds(290, 320, 600, 35);
             
-            contentPane.add(l6);
-            contentPane.add(l11);
-            contentPane.add(l12);
-            contentPane.add(l13);
+            JLabel l5 = new JLabel("Developed By : ");
+            l5.setForeground(new Color(127, 255, 0));
+            l5.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 34));
+            l5.setBounds(130,180,405,40);
+            contentPane.add(l5);
 
-            JLabel l7 = new JLabel("Contact Us : yash.gupta_cs19@gla.ac.in");
-            l7.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            JLabel l14 = new JLabel("yash.nigam_cs19@gla.ac.in");
-            l14.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            JLabel l15 = new JLabel("rajat.pandey_cs19@gla.ac.in");
-            l15.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            JLabel l16 = new JLabel("pawan.pandey_cs19@gla.ac.in");
-            l16.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            l7.setBounds(70, 400, 600, 34);
-            l14.setBounds(190, 450, 600, 34);
-            l15.setBounds(190, 500, 600, 34);
-            l16.setBounds(190, 550, 600, 34);
-            contentPane.add(l7);
-            contentPane.add(l14);
-            contentPane.add(l15);
-            contentPane.add(l16);        
-            contentPane.setBackground(Color.WHITE);
-	}
-        
+         
+        linkEmail1.setURL("mailto:pawan.pandey_cs19@gla.ac.in");
+        linkEmail2.setURL("mailto:yash.nigam_cs19@gla.ac.in");
+        linkEmail3.setURL("mailto:yash.gupta_cs19@gla.ac.in");
+        linkEmail4.setURL("mailto:rajat.pandey_cs19@gla.ac.in");
+        linkEmail1.setBounds(380,180,405,40);
+        linkEmail1.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+        linkEmail2.setBounds(390,255,405,40);
+        linkEmail2.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+        linkEmail3.setBounds(390,335,405,40);
+        linkEmail3.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+        linkEmail4.setBounds(380,410,405,40);
+        linkEmail4.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
+         
+        getContentPane().add(linkEmail1);
+        getContentPane().add(linkEmail2);
+        getContentPane().add(linkEmail3);
+        getContentPane().add(linkEmail4);
 
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+    }
+ 
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+             
+            @Override
+            public void run() {
+                new aboutUs().setVisible(true);
+            }
+        });
+    }
 }
